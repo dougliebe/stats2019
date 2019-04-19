1798682d7d69512893ff633894c122b636b23dca
dougliebe <liebe.doug@gmail.com>
2019-04-19 10:11:53 -0400

    adding lmer model

--- role_clusters.R
+++ role_clusters.R
@@ -96,7 +96,7 @@ hpdata <- data %>%
          HillTime=(hill.time..s.*60)/(duration..s.),
          APM = (assists*60)/(duration..s.))%>%
         # KPM = (kills*60)/(duration..s.))%>%
-  select(player,team,EPM,SPM,DeathPM,HillTime,APM)
+  select(player,team,EPM,SPM,DeathPM,HillTime)
 
 #Clustering Kmeans------
 data <- data %>%
@@ -114,7 +114,7 @@ data <- data %>%
          fdpr = snd.firstdeaths/ifelse(snd.rounds == 0,duration..s.,snd.rounds)) 
 player_group<-data%>%
   filter(mode=="Hardpoint")%>%
-  group_by(player,team) %>%
+  group_by(player) %>%
   summarise(EPM = round(sum(EngPlus, na.rm = T)/
                           (sum(duration..s.,na.rm =T))*60,6),
             SPM = round(sum(player.score, na.rm = T)/
@@ -126,31 +126,84 @@ player_group<-data%>%
             HillTime=round(mean(hill.time..s.,na.rm = T),1),
             APM=round(sum(assists,na.rm = T)/(sum(duration..s.,na.rm =T))*60,6))%>%
            # KPM=round(sum(kills,na.rm=T)/(sum(duration..s.,na.rm =T))*60,6))%>%
-  select(player,SPM,EPM,DeathPM,HillTime,APM)%>%
+  select(player,SPM,EPM,DeathPM,HillTime)%>%
   arrange(desc(SPM))%>%
   data.frame()
 
 hpcluster<-hpdata[,c(-1,-2)]
 hpclusterScaled<-scale(hpcluster)
-
+set.seed(1)
 fitK<-kmeans(hpclusterScaled,6)
 fitK
-plot(hpcluster,col=fitK$cluster)
+# plot(hpcluster,col=fitK$cluster)
+# 
+# k<-list()
+# for(i in 1:10){
+#   k[[i]]<-kmeans(hpclusterScaled,i)
+# }
+# 
+# betweenss_totss<-list()
+# for(i in 1:10){
+#   betweenss_totss[[i]]<-k[[i]]$betweenss/k[[i]]$totss
+# }
+# plot(1:10,betweenss_totss,type="b",ylab="between ss/total ss",xlab="clusters (k)")
 
-k<-list()
-for(i in 1:10){
-  k[[i]]<-kmeans(hpclusterScaled,i)
-}
-
-betweenss_totss<-list()
-for(i in 1:10){
-  betweenss_totss[[i]]<-k[[i]]$betweenss/k[[i]]$totss
-}
-plot(1:10,betweenss_totss,type="b",ylab="between ss/total ss",xlab="clusters (k)")
-
-player_group$group<-predict(as.kcca(fitK, data = hpclusterScaled), scale(player_group[c(-1)]))
+player_group$group<-predict(as.kcca(fitK, data = hpclusterScaled), scale(player_group[-1]))
 
 plot(player_group[-1], col = player_group$group)
+
+#add overall group to data
+data <- merge(data, player_group[,c('player','group')], by = 'player')
+data$group <- as.factor(data$group)
+
+library(lme4)
+library(lmerTest)
+library(ggplot2)
+total <- lme4::glmer(win. ~ scale(spr):mode + 
+                          scale(dpr):mode + 
+                          scale(dmgpr):mode +
+                          scale(apr):mode + 
+                          # scale(kpr):mode + 
+                          (mode|group),
+                        data %>% filter(!is.na(spr)), family = binomial(link='logit'))
+summary(total)
+ranef(total)
+
+data %>%
+  mutate(pred = predict(total, data, type = "response")) %>%
+  mutate(correct = ifelse((pred < 0.5 & win. == "L") | (pred > 0.5 & win. == "W"),1,0 )) %>%
+  group_by(mode) %>%
+  summarise(p = mean(correct, na.rm = T))
+
+
+  data.frame() %>%
+  # ggplot(aes(pred, color = mode))+geom_density()
+
+
+
+
+
+#list sheets
+gs_ls()
+# get stats sheet
+pull <- gs_title('Rosters')
+# list worksheets
+gs_ws_ls(pull)
+#get data just from Sheet1
+roster <- as.data.frame(gs_read(ss=pull, ws = "Sheet1"))
+
+final <- merge(player_group, roster[-3], by.x = "player", by.y = "Player", all.x = T)
+library(reshape2)
+final %>%
+  group_by(Team, group) %>%
+  summarise(count = n()) %>%
+  dcast(Team~group)
+
+final %>%
+  group_by(group) %>%
+  filter(group == 4) %>% 
+  select(player)
+  summarise(count = n())
 
 groups<-player_group%>%
   arrange(desc(group))%>%
